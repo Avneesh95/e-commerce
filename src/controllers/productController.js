6dc2d4f664c88388f30cf5d2239dce5eccf7f0c3
Avneesh <avneesh952003@gmail.com>
2026-06-23 21:33:59 +0530

    product creation done

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -25,7 +25,52 @@ const createProduct = async (req, res) => {
 }
 
 
+const products = async (req, res) => {
+
+ try {
+  const product =await productModel.find();
+
+  res.status(200).json({
+    success:true,
+    count : product.length,
+    product
+
+ });
+ }
+ catch(error){
+  res.status(500).json({
+      success: false,
+      error: error.message,
+    });
+  }
+ 
+}
+
+
+const getProductById = async (req, res) => {
+  const { id } = req.params;
+
+  const product = await productModel.findById(id);
+
+  if (!product) {
+    return res.status(404).json({
+      success: false,
+      message: "Product not found",
+    });
+  }
+
+  res.status(200).json({
+    success: true,
+    product
+  });
+}
+
+
+
+
 
 module.exports = {
-  createProduct
+  createProduct,
+  products,
+  getProductById
 };
--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
-const { createProduct } = require("../controllers/productController");
+const { createProduct ,products , getProductById} = require("../controllers/productController");
 
 
 router.post("/", createProduct);
+
+router.get('/',products);
+
+router.get("/:id", getProductById);
 
 module.exports = router;
